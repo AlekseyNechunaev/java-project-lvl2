6f18cd3bb0c305320e66acbd3b5ea3b9b6254db2
Aleksey Nechunaev <aleksey.nechunaev@gmail.com>
2022-08-08 23:50:58 +0700

    add parameters filePath

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,11 +4,22 @@ import hexlet.code.serivces.ProjectInfoConst;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.io.File;
+
 
 @Command(name = ProjectInfoConst.NAME, mixinStandardHelpOptions = true,
         version = ProjectInfoConst.NAME + " " + ProjectInfoConst.VERSION,
         description = ProjectInfoConst.DESCRIPTION)
 public class App implements Runnable {
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File pathToFirstFile;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File pathToSecondFile;
+
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
     public static void main(String[] args) {
          int exitCode = new CommandLine(new App()).execute(args);
          System.exit(exitCode);
--- src/main/java/hexlet/code/serivces/ProjectInfoConst.java
+++ src/main/java/hexlet/code/serivces/ProjectInfoConst.java
@@ -3,5 +3,5 @@ package hexlet.code.serivces;
 public class ProjectInfoConst {
     public static final String NAME = "gendiff";
     public static final String VERSION = "1.0";
-    public static final String DESCRIPTION = "Compares two configuration files and shows a difference";
+    public static final String DESCRIPTION = "Compares two configuration files and shows a difference.";
 }
