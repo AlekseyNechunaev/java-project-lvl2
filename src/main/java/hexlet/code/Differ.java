3cb2daebeb53b6448473b3cedf158e27b62b2cdc
Aleksey Nechunaev <aleksey.nechunaev@gmail.com>
2022-08-12 20:07:02 +0700

    fix

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Differ {
 
